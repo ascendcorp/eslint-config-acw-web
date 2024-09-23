3a49a01cbd451434d3f83fbf37754e8e8658fe0a
Supawish kaewjing <supawish.kj@gmail.com>
2024-09-23 10:29:17 +0700

    feat: migrate to support eslint version 9

--- Makefile
+++ Makefile
@@ -1,3 +1,15 @@
+release-major:
+	npx standard-version --release-as major
+	git push --follow-tags origin main && npm publish
+
+release-minor:
+	npx standard-version --release-as minor
+	git push --follow-tags origin main && npm publish
+
+release-patch:
+	npx standard-version --release-as patch
+	git push --follow-tags origin main && npm publish
+
 release:
 	npx standard-version
 	git push --follow-tags origin main && npm publish
--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const { resolve } = require("node:path")
-
-const project = resolve(process.cwd(), "tsconfig.json")
+// const typescriptSortKeys = require("eslint-plugin-typescript-sort-keys")
 
 module.exports = {
-	root: true,
-	extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react/jsx-runtime", "plugin:import/recommended", "plugin:import/typescript", "plugin:@typescript-eslint/strict-type-checked", "plugin:@typescript-eslint/stylistic-type-checked", "plugin:typescript-sort-keys/recommended", "prettier"],
-	parser: "@typescript-eslint/parser",
-	parserOptions: { project },
-	settings: {
-		"import/parsers": { "@typescript-eslint/parser": [".js", ".jsx", ".ts", ".tsx"] },
-		"import/resolver": { typescript: { project }, node: true },
-		"react": { version: "detect" },
-	},
-	plugins: ["@typescript-eslint", "import", "react", "react-hooks", "prettier", "typescript-sort-keys"],
 	rules: {
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/no-misused-promises": "off",
+		"@typescript-eslint/prefer-nullish-coalescing": "off",
+		"@typescript-eslint/unbound-method": "off",
 		"@typescript-eslint/array-type": "error",
 		"@typescript-eslint/await-thenable": "error",
 		"@typescript-eslint/consistent-indexed-object-style": "error",
 		"@typescript-eslint/consistent-type-definitions": "error",
 		"@typescript-eslint/consistent-type-imports": "error",
 		"@typescript-eslint/dot-notation": "error",
-		"@typescript-eslint/no-confusing-void-expression": ["error", { ignoreArrowShorthand: true }], // NOTE: already in strict-type-checked but without ignoreArrowShorthand=true
+		"@typescript-eslint/no-confusing-void-expression": [
+			"error",
+			{
+				ignoreArrowShorthand: true,
+			},
+		],
 		"@typescript-eslint/no-dynamic-delete": "error",
 		"@typescript-eslint/no-floating-promises": "error",
-		"@typescript-eslint/no-misused-promises": ["error", { checksVoidReturn: { attributes: false } }],
+
+		"@typescript-eslint/no-misused-promises": [
+			"error",
+			{
+				checksVoidReturn: {
+					attributes: false,
+				},
+			},
+		],
 		"@typescript-eslint/no-shadow": "error",
 		"@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
 		"@typescript-eslint/no-unsafe-argument": "off",
@@ -45,27 +50,31 @@ module.exports = {
 		"array-callback-return": "error",
 		"complexity": "error",
 		"eqeqeq": ["error", "always"],
-		"import/export": "warn",
-		"import/no-anonymous-default-export": "error",
-		"import/no-mutable-exports": "error",
-		"import/no-named-as-default": "off",
-		"import/no-named-as-default-member": "off",
-		"import/no-unused-modules": ["error", { unusedExports: true }],
-		"import/order": ["error", { "groups": ["builtin", "external", "index", "parent", "sibling"], "newlines-between": "always", "alphabetize": { order: "asc", caseInsensitive: true, orderImportKind: "asc" } }],
 		"max-depth": "error",
-		"max-lines": ["error", { skipBlankLines: true, skipComments: true }],
-		"max-params": ["error", 3], // voted: https://truemoney.atlassian.net/wiki/spaces/ACWMF/pages/2266660887/Code+Conventional#function-max-params-=-3
+		"max-lines": [
+			"error",
+			{
+				skipBlankLines: true,
+				skipComments: true,
+			},
+		],
+		"max-params": ["error", 3],
 		"member-access": "off",
-		"no-else-return": ["error", { allowElseIf: false }],
+		"no-else-return": [
+			"error",
+			{
+				allowElseIf: false,
+			},
+		],
 		"no-nested-ternary": "error",
 		"no-param-reassign": "error",
 		"no-redeclare": "error",
-		"no-shadow": "off", // conflict with @typescript-eslint
+		"no-shadow": "off",
 		"no-unneeded-ternary": "error",
 		"no-unreachable": "error",
 		"no-unused-expressions": "error",
-		"no-unused-vars": "off", // conflict with @typescript-eslint
-		"no-use-before-define": "off", // conflict with @typescript-eslint
+		"no-unused-vars": "off",
+		"no-use-before-define": "off",
 		"no-useless-computed-key": "error",
 		"no-useless-concat": "error",
 		"no-useless-rename": "error",
@@ -76,19 +85,58 @@ module.exports = {
 		"prettier/prettier": "error",
 		"react-hooks/exhaustive-deps": "error",
 		"react-hooks/rules-of-hooks": "error",
-		"react/destructuring-assignment": ["error", "always", { destructureInSignature: "always" }],
+		"react/destructuring-assignment": [
+			"error",
+			"always",
+			{
+				destructureInSignature: "always",
+			},
+		],
 		"react/display-name": "off",
 		"react/jsx-boolean-value": "error",
 		"react/jsx-curly-brace-presence": "error",
 		"react/jsx-key": "error",
 		"react/jsx-pascal-case": "error",
-		"react/jsx-sort-props": ["error", { ignoreCase: true, reservedFirst: true }],
+		"react/jsx-sort-props": [
+			"error",
+			{
+				ignoreCase: true,
+				reservedFirst: true,
+			},
+		],
 		"react/no-multi-comp": "error",
 		"react/no-unused-prop-types": "error",
 		"react/prop-types": "off",
 		"react/self-closing-comp": "error",
-		"typescript-sort-keys/interface": "error",
-		"typescript-sort-keys/string-enum": "error",
+
+		// "typescript-sort-keys/interface": "error",
+		// "typescript-sort-keys/string-enum": "error",
+
+		// "import/export": "warn",
+		// "import/no-anonymous-default-export": "error",
+		// "import/no-mutable-exports": "error",
+		// "import/no-named-as-default": "off",
+		// "import/no-named-as-default-member": "off",
+
+		// "import/no-unused-modules": [
+		// 	"error",
+		// 	{
+		// 		unusedExports: true,
+		// 	},
+		// ],
+
+		// "import/order": [
+		// 	"error",
+		// 	{
+		// 		"groups": ["builtin", "external", "index", "parent", "sibling"],
+		// 		"newlines-between": "always",
+
+		// 		"alphabetize": {
+		// 			order: "asc",
+		// 			caseInsensitive: true,
+		// 			orderImportKind: "asc",
+		// 		},
+		// 	},
+		// ],
 	},
-	env: { browser: true, node: true, jest: true },
 }
